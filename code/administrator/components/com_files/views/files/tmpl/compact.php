6ccf22f0549553466b84d2ba5bfe2d0d0a53e01b
Arunas Mazeika <arunas@timble.net>
2013-06-19 16:38:27 +0200

    Set cookie path during instantiation.
    re #27

--- code/administrator/components/com_files/views/files/tmpl/compact.php
+++ code/administrator/components/com_files/views/files/tmpl/compact.php
@@ -23,6 +23,9 @@ Files.token = '<?= $token; ?>';
 window.addEvent('domready', function() {
 	var config = <?= json_encode($state->config); ?>,
 		options = {
+            cookie: {
+                path: '<?=$this->getView()->baseurl?>'
+            },
             pathway: false,
 			state: {
 				defaults: {
