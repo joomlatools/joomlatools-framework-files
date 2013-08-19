ac202bdc36706c3e0ee0bd2e8a71d55fa1c544d8
Ercan Ozkaya <ercan@timble.net>
2013-08-19 23:32:25 +0300

    re #91: KObjectManager methods are not static anymore.
    Use KObjectManager::getInstance to grab the singleton object

--- code/libraries/koowa/components/com_files/models/state.php
+++ code/libraries/koowa/components/com_files/models/state.php
@@ -33,7 +33,7 @@ class ComFilesModelState extends KModelState
 
         if ($name === 'container' && is_string($result))
         {
-            $result = KObjectManager::getObject('com://admin/files.model.containers')->slug($result)->getItem();
+            $result = KObjectManager::getInstance()->getObject('com://admin/files.model.containers')->slug($result)->getItem();
 
 	        if (!is_object($result) || $result->isNew()) {
 	            throw new UnexpectedValueException('Invalid container');
