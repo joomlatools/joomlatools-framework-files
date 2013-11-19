1d9bf3cb698511010d372f5ad3c5f0630608bddd
Ercan Ozkaya <ercan@timble.net>
2013-11-19 12:31:14 +0100

    Fix SSL config in Curl call

--- code/libraries/koowa/components/com_files/database/row/url.php
+++ code/libraries/koowa/components/com_files/database/row/url.php
@@ -87,7 +87,6 @@ class ComFilesDatabaseRowUrl extends KDatabaseRowAbstract
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
