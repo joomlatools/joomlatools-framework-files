c4bc44d636a9e647924c094997c5ebb817216ced
Ercan Ozkaya <ercan@timble.net>
2014-01-09 17:36:54 +0200

    Clean up paginator

--- code/libraries/koowa/components/com_files/template/helper/paginator.php
+++ code/libraries/koowa/components/com_files/template/helper/paginator.php
@@ -29,36 +29,20 @@ class ComFilesTemplateHelperPaginator extends ComKoowaTemplateHelperPaginator
             'limit'   => 0,
         ));
 
-        $current_of_total = $this->translate('JLIB_HTML_PAGE_CURRENT_OF_TOTAL');
-        if($current_of_total == 'JLIB_HTML_PAGE_CURRENT_OF_TOTAL'
-            // Awful fix for Joomla language debug adding ?? before the string
-            || strpos(substr($current_of_total, 2), 'JLIB_HTML_PAGE_CURRENT_OF_TOTAL') !== false
-        ) {
-            $current_of_total = 'Page %s of %s';
-        }
-
         $html  = '<div class="container" id="files-paginator-container">';
 
-        if(version_compare(JVERSION, '3.0', '>=')) {
-            $html .= '<div class="pagination pagination-toolbar" id="files-paginator">';
-        } else {
-            $html .= '<div class="pagination pagination-legacy" id="files-paginator">';
-        }
+        $html .= '<div class="pagination pagination-toolbar" id="files-paginator">';
 
         $html .= '<div class="limit">'.$this->limit($config->toArray()).'</div>';
 
-        if(version_compare(JVERSION, '3.0', '>='))
-        {
-            $html .= '<span class="start hidden"><a></a></span>';
-            $html .= '<ul class="pagination-list">';
-            $html .=  $this->_pages();
-            $html .= '</ul>';
-            $html .= '<span class="end hidden"><a></a></span>';
-        }
-        else $html .=  $this->_pages();
+        $html .= '<span class="start hidden"><a></a></span>';
+        $html .= '<ul class="pagination-list">';
+        $html .=  $this->_pages();
+        $html .= '</ul>';
+        $html .= '<span class="end hidden"><a></a></span>';
 
         $html .= '<div class="limit pull-right"> ';
-        $html .= sprintf($current_of_total, '<strong class="page-current">1</strong>', '<strong class="page-total">1</strong></div>');
+        $html .= sprintf($this->translate('JLIB_HTML_PAGE_CURRENT_OF_TOTAL'), '<strong class="page-current">1</strong>', '<strong class="page-total">1</strong></div>');
         $html .= '</div>';
         $html .= '</div>';
 
@@ -68,32 +52,19 @@ class ComFilesTemplateHelperPaginator extends ComKoowaTemplateHelperPaginator
     /**
      * Render a list of pages links
      *
-     * This function is overriddes the default behavior to render the links in the khepri template
+     * This function is overrides the default behavior to render the links in the khepri template
      * backend style.
      *
-     * @param   array   An array of page data
+     * @param   array   $pages An array of page data
      * @return  string  Html
      */
     protected function _pages($pages = null)
     {
-        if(version_compare(JVERSION, '3.0', '>='))
-        {
-            $tpl = '<li class="%s"><a href="#">%s</a></li>';
+        $tpl = '<li class="%s"><a href="#">%s</a></li>';
 
-            $html  = sprintf($tpl, 'prev', '&larr;');
-            $html .= '<li class="page"></li>';
-            $html .= sprintf($tpl, 'next', '&rarr;');
-        }
-        else
-        {
-            $tpl = '<div class="button2-%s"><div class="%s"><a href="#">%s</a></div></div>';
-
-            $html = sprintf($tpl, 'right', 'start', $this->translate('Start'));
-            $html .= sprintf($tpl, 'right', 'prev', $this->translate('Prev'));
-            $html .= '<div class="button2-left"><div class="page"></div></div>';
-            $html .= sprintf($tpl, 'left', 'next', $this->translate('Next'));
-            $html .= sprintf($tpl, 'left', 'end', $this->translate('End'));
-        }
+        $html  = sprintf($tpl, 'prev', '&larr;');
+        $html .= '<li class="page"></li>';
+        $html .= sprintf($tpl, 'next', '&rarr;');
 
         return $html;
     }
