a749497f0642e38e0f5c6d5e5156ebedc90a7fec
Ercan Ozkaya <ercan@timble.net>
2014-01-21 15:06:19 +0200

    Consolidate compact layouts into com_files

--- code/libraries/koowa/components/com_files/views/files/tmpl/compact.html.php
+++ code/libraries/koowa/components/com_files/views/files/tmpl/compact.html.php
@@ -6,13 +6,14 @@
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://github.com/joomlatools/koowa-files for the canonical source repository
  */
-defined('KOOWA') or die( 'Restricted access' ); ?>
+defined('KOOWA') or die;
+
+$can_upload = @object('user')->authorise('com_docman.upload', 'com_docman');
+?>
 
 <?= @import('com:files.files.scripts.html'); ?>
 
 <script src="media://koowa/com_files/js/files.compact.js" />
-
-<style src="media://koowa/com_files/css/files.css" />
 
 <script>
 Files.sitebase = '<?= $sitebase; ?>';
@@ -34,6 +35,9 @@ window.addEvent('domready', function() {
 			editor: <?= json_encode($state->editor); ?>,
 			types: <?= json_encode($state->types); ?>,
 			container: <?= json_encode($container ? $container->toArray() : null); ?>,
+            onBeforeSetContainer: function(container) {
+                container.title = <?= json_encode('Root') ?>;
+            },
             uploader_dialog: false,
             folder_dialog: false
 		};
@@ -41,78 +45,157 @@ window.addEvent('domready', function() {
 
 	Files.app = new Files.Compact.App(options);
 
-	$$('#tabs-pane_insert dt').addEvent('click', function(){
-		setTimeout(function(){window.fireEvent('refresh');}, 300);
-	});
+    <? if ($can_upload): ?>
+    $('files-new-folder-create').addEvent('click', function(e){
+        e.stop();
+
+        var element = $('files-new-folder-input'),
+            value = element.get('value');
+
+        if (value.length > 0) {
+            var folder = new Files.Folder({name: value, folder: Files.app.getPath()});
+
+            folder.add(function(response, responseText) {
+                var el = response.entities[0],
+                    cls = Files[el.type.capitalize()],
+                    row = new cls(el);
+
+                element.set('value', '');
+                $('files-new-folder-create').removeClass('valid').setProperty('disabled', 'disabled');
+
+                Files.app.tree.appendNode({
+                    id: row.path,
+                    label: row.name
+                });
+            });
+        }
+    });
+    var validate = function(){
+            if(this.value.trim()) {
+                $('files-new-folder-create').addClass('valid').removeProperty('disabled');
+            } else {
+                $('files-new-folder-create').removeClass('valid').setProperty('disabled', 'disabled');
+            }
+        },
+        input = $('files-new-folder-input');
+
+    input.addEvent('change', validate);
+
+    if (window.addEventListener) {
+        input.addEventListener('input', validate);
+    } else {
+        input.addEvent('keyup', validate);
+    }
+    <? endif; ?>
+});
+
+kQuery(function($) {
+
+    // Set initially
+    $('.koowa_dialog__file_dialog_upload').hide();
+
+    $('.koowa_dialog__menu__child--insert')
+        .addClass('active')
+        .click(function() {
+            $(this).addClass('active')
+                .siblings().removeClass('active');
+
+            $('.koowa_dialog__file_dialog_upload').hide();
+
+            $('.koowa_dialog__file_dialog_files, .koowa_dialog__file_dialog_insert').show();
+        });
+
+    $('.koowa_dialog__menu__child--download').click(function() {
+        $(this).addClass('active')
+            .siblings().removeClass('active');
+
+        $('.koowa_dialog__file_dialog_files, .koowa_dialog__file_dialog_insert').hide();
+        $('.koowa_dialog__file_dialog_upload').show();
+    });
+
+    if ( $('body').width() <= '800' ) {
+        $('a.koowa_dialog__menu__child--download').click(function() {
+            $('html, body').animate({
+                scrollTop: $("#koowa_dialog__file_dialog_upload").offset().top
+            }, 1000);
+        });
+    }
+
 });
 </script>
 
 <?= @import('com:files.files.templates_compact.html');?>
 
-<div class="docman_dialog docman_dialog--file_dialog">
-    <div class="docman_dialog__menu">
-        <a class="docman_dialog__menu__child--insert"><?= @translate('Select'); ?></a>
-        <a class="docman_dialog__menu__child--download"><?= @translate('Upload'); ?></a>
+<div class="koowa_dialog koowa_dialog--file_dialog">
+    <div class="koowa_dialog__menu">
+        <a class="koowa_dialog__menu__child--insert"><?= @translate('Select'); ?></a>
+        <? if ($can_upload): ?>
+            <a class="koowa_dialog__menu__child--download"><?= @translate('Upload'); ?></a>
+        <? endif; ?>
     </div>
-    <div class="docman_dialog__layout">
-        <div class="docman_dialog__wrapper">
-            <div class="docman_dialog__wrapper__child docman_dialog__file_dialog_categories">
-                <h2 class="docman_dialog__title">
+    <div class="koowa_dialog__layout">
+        <div class="koowa_dialog__wrapper">
+            <div class="koowa_dialog__wrapper__child koowa_dialog__file_dialog_categories">
+                <h2 class="koowa_dialog__title">
                     <?= @translate('Select a folder'); ?>
                 </h2>
-                <div class="docman_dialog__child__content docman_dialog__folders_files">
-                    <div class="docman_dialog__child__content__box">
-                        <div class="docman_dialog__files_tree" id="files-tree" style="overflow: auto; height: auto;"></div>
+                <div class="koowa_dialog__child__content koowa_dialog__folders_files">
+                    <div class="koowa_dialog__child__content__box">
+                        <div class="koowa_dialog__files_tree" id="files-tree" style="overflow: auto; height: auto;"></div>
                     </div>
-                    <div class="docman_dialog__new_folder">
-                        <h2 class="docman_dialog__title docman_dialog__title--child">
-                            <?= @translate('Create a new folder'); ?>
-                        </h2>
-                        <div class="docman_dialog__block" id="files-new-folder-modal">
-                            <div class="input-group" style="margin:0;">
-                                <input type="text" class="input-group-form-control" id="files-new-folder-input" />
-                        <span class="input-group-btn">
-                            <button id="files-new-folder-create" class="btn" disabled><?= @translate('Add folder'); ?></button>
-                        </span>
+                    <? if ($can_upload): ?>
+                        <div class="koowa_dialog__new_folder">
+                            <h2 class="koowa_dialog__title koowa_dialog__title--child">
+                                <?= @translate('Create a new folder'); ?>
+                            </h2>
+                            <div class="koowa_dialog__block" id="files-new-folder-modal">
+                                <div class="input-group" style="margin:0;">
+                                    <input type="text" class="input-group-form-control" id="files-new-folder-input" />
+                            <span class="input-group-btn">
+                                <button id="files-new-folder-create" class="btn" disabled><?= @translate('Add folder'); ?></button>
+                            </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <? endif; ?>
                 </div>
             </div>
 
-            <div class="docman_dialog__wrapper__child docman_dialog__file_dialog_files">
-                <h2 class="docman_dialog__title">
+            <div class="koowa_dialog__wrapper__child koowa_dialog__file_dialog_files">
+                <h2 class="koowa_dialog__title">
                     <?= @translate('Select a file'); ?>
                 </h2>
-                <div class="docman_dialog__child__content">
-                    <div class="docman_dialog__child__content__box">
+                <div class="koowa_dialog__child__content">
+                    <div class="koowa_dialog__child__content__box">
                         <div id="files-grid">
 
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="docman_dialog__wrapper__child docman_dialog__file_dialog_insert">
-                <h2 class="docman_dialog__title">
+            <div class="koowa_dialog__wrapper__child koowa_dialog__file_dialog_insert">
+                <h2 class="koowa_dialog__title">
                     <?= @translate('Selected file info'); ?>
                 </h2>
-                <div class="docman_dialog__child__content">
-                    <div class="docman_dialog__child__content__box">
+                <div class="koowa_dialog__child__content">
+                    <div class="koowa_dialog__child__content__box">
                         <div id="files-preview"></div>
                         <div id="insert-button-container"></div>
                     </div>
                 </div>
             </div>
-            <div id="docman_dialog__file_dialog_upload" class="docman_dialog__wrapper__child docman_dialog__file_dialog_upload">
-                <h2 class="docman_dialog__title">
+            <? if ($can_upload): ?>
+            <div id="koowa_dialog__file_dialog_upload" class="koowa_dialog__wrapper__child koowa_dialog__file_dialog_upload">
+                <h2 class="koowa_dialog__title">
                     <?= @translate('Upload files to docman'); ?>
                 </h2>
-                <div class="docman_dialog__child__content">
-                    <div class="docman_dialog__child__content__box">
+                <div class="koowa_dialog__child__content">
+                    <div class="koowa_dialog__child__content__box">
                         <?= @import('com:files.files.uploader.html', array('multi_selection' => false)); ?>
                     </div>
                 </div>
             </div>
+            <? endif; ?>
         </div>
     </div>
 </div>
