0004f2a7a175e6303979d8c975f204682a91a84c
Ercan Ozkaya <ercan@timble.net>
2014-05-22 12:03:04 +0200

    Update copyright information

--- code/libraries/koowa/components/com_files/database/exception/remoteadapternotavailable.php
+++ code/libraries/koowa/components/com_files/database/exception/remoteadapternotavailable.php
@@ -2,7 +2,7 @@
 /**
  * Koowa Framework - http://developer.joomlatools.com/koowa
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://github.com/joomlatools/koowa-files for the canonical source repository
  */
@@ -10,8 +10,7 @@
 /**
  * Remote Adapter Not Available Database Exception
  *
- * @author      Ercan Ozkaya <http://nooku.assembla.com/profile/ercanozkaya>
- * @package     Nooku_Components
- * @subpackage  Files
+ * @author  Ercan Ozkaya <https://github.com/ercanozkaya>
+ * @package Koowa\Component\Files
  */
 class ComFilesDatabaseExceptionRemoteAdapterNotAvailable extends Exception {}
