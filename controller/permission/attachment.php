1cf14d68a18774212d7512c636adf3d969416466
Arunas Mazeika <arunas@timble.net>
2015-12-02 11:25:42 +0100

    re #83 Added attachment controller permission class.

---
+++ controller/permission/attachment.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright	Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ */
+
+/**
+ * Attachment Controller Permission
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesControllerPermissionAttachment extends KControllerPermissionAbstract
+{
+}
