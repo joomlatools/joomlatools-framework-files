587bdc1374ebb4dc948b508cd8148f549e1876b3
Arunas Mazeika <arunas@timble.net>
2016-01-28 14:27:41 +0100

    re #83 Added attachments JSON view.

---
+++ view/attachments/json.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright	Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ */
+
+/**
+ * Attachments JSON View.
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesViewAttachmentsJson extends ComFilesViewJson
+{
+    protected function _getEntity(KModelEntityInterface $entity)
+    {
+        $data = parent::_getEntity($entity);
+
+        $data['file'] = $entity->file->toArray();
+        $data['created_on_timestamp'] = strtotime($entity->created_on);
+
+        return $data;
+    }
+}
