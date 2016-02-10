7bfe7a2667827b89bc5914e7c668cd6791451b99
Arunas Mazeika <arunas@timble.net>
2016-02-10 11:04:51 +0100

    re #83 Cleanup.
    All relations logic got moved to the relatable behavior.

--- model/attachments.php
+++ model/attachments.php
@@ -15,67 +15,17 @@
  */
 class ComFilesModelAttachments extends KModelDatabase
 {
-    protected $_relations_model;
-
-    public function __construct(KObjectConfig $config)
-    {
-        parent::__construct($config);
-
-        $this->_relations_model = $config->relations_model;
-
-        $this->getState()->insert('table', 'cmd')->insert('row', 'cmd');
-    }
-
     protected function _initialize(KObjectConfig $config)
     {
-        $config->append(array('relations_model' => 'attachments_relations'));
+        $config->append(array('behaviors' => array('attachable')));
         parent::_initialize($config);
-    }
-
-    public function getRelationsModel()
-    {
-        if (!$this->_relations_model instanceof KModelInterface)
-        {
-            $identifier = $this->_relations_model;
-
-            if (is_string($identifier))
-            {
-                if (strpos($identifier, '.') === false)
-                {
-                    $identifier = $this->getIdentifier()->toArray();
-                    $identifier['name'] = $this->_relations_model;
-                }
-
-                $identifier = $this->getIdentifier($identifier);
-            }
-
-            $this->_relations_model = $this->getObject($identifier, array(
-                'relation_column' => $this->getTable()
-                                          ->getIdentityColumn()
-            ));
-        }
-
-        return $this->_relations_model;
     }
 
     protected function _buildQueryColumns(KDatabaseQueryInterface $query)
     {
         parent::_buildQueryColumns($query);
 
-        $columns = array('tbl.*', 'container_slug' => 'containers.slug');
-
-        $state = $this->getState();
-
-        if ($state->table && $state->row)
-        {
-            $columns = array_merge($columns, array(
-                'attached_by'      => 'relations.created_by',
-                'attached_on'      => 'relations.created_on',
-                'attached_by_name' => 'users.name'
-            ));
-        }
-
-        $query->columns($columns);
+        $query->columns(array('tbl.*', 'container_slug' => 'containers.slug'));
     }
 
     protected function _buildQueryJoins(KDatabaseQueryInterface $query)
@@ -83,17 +33,6 @@ class ComFilesModelAttachments extends KModelDatabase
         parent::_buildQueryJoins($query);
 
         $query->join('files_containers AS containers', 'containers.files_container_id = tbl.files_container_id', 'INNER');
-
-        $state = $this->getState();
-
-        if ($state->row || $state->table)
-        {
-            $table  = $this->getRelationsModel()->getTable()->getBase();
-            $column = $this->getTable()->getIdentityColumn();
-
-            $query->join($table . ' AS relations', 'relations.' . $column . ' = tbl.' . $column, 'INNER')
-                  ->join('users AS users', 'relations.created_by = users.id', 'LEFT');
-        }
     }
 
     protected function _buildQueryWhere(KDatabaseQueryInterface $query)
@@ -114,15 +53,6 @@ class ComFilesModelAttachments extends KModelDatabase
             }
 
             $query->bind(array('container' => $container, 'name' => $name));
-
-        }
-
-        if ($row = $state->row) {
-            $query->where('relations.row = :row')->bind(array('row' => $row));
-        }
-
-        if ($table = $state->table) {
-            $query->where('relations.table = :table')->bind(array('table' => $table));
         }
     }
 
