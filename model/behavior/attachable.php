29e966ef91402ce10cfc94708ec42e416b7d1548
Arunas Mazeika <arunas@timble.net>
2016-02-10 11:04:25 +0100

    re #83 Added attachable behavior.

---
+++ model/behavior/attachable.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright	Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ */
+
+/**
+ * Attachable Model behavior
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesModelBehaviorAttachable extends ComFilesModelBehaviorRelatable
+{
+    /**
+     * Overridden to include creatable info.
+     */
+    protected function _beforeFetch(KModelContextInterface $context)
+    {
+        parent::_beforeFetch($context);
+
+        $state = $context->getState();
+        $query = $context->query;
+
+        if ($state->table && $state->row)
+        {
+            $query->join('users AS users', 'relations.created_by = users.id', 'LEFT');
+
+            $query->columns(array(
+                'attached_by'      => 'relations.created_by',
+                'attached_on'      => 'relations.created_on',
+                'attached_by_name' => 'users.name'
+            ));
+        }
+    }
+}
