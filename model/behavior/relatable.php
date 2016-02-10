5e6d5c88652f400c579096841a3e107fcbf66e89
Arunas Mazeika <arunas@timble.net>
2016-02-10 11:04:14 +0100

    re #83 Added relatable behavior.

---
+++ model/behavior/relatable.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright	Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ */
+
+/**
+ * Relatable Model behavior
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesModelBehaviorRelatable extends KModelBehaviorAbstract
+{
+    /**
+     * Relations model.
+     *
+     * @var KModelInterface|string|KObjectIdentifierInterface
+     */
+    protected $_model;
+
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_model = $config->model;
+        $this->_columns = KObjectConfig::unbox($config->columns);
+    }
+
+    protected function _initialize(KObjectConfig $config)
+    {
+        if ($mixer = $config->mixer) {
+            $model = sprintf('%s_%s', $mixer->getIdentifier()->getName(), 'relations');
+        } else {
+            $model = 'relations';
+        }
+
+        $config->append(array('model' => $model, 'columns' => array('table' => 'cmd', 'row' => 'cmd')));
+        parent::_initialize($config);
+    }
+
+    /**
+     * Before Fetch command handler.
+     *
+     * Adds joins and where statements.
+     *
+     * @param KModelContextInterface $context The context object.
+     */
+    protected function _beforeFetch(KModelContextInterface $context)
+    {
+        $query = $context->query;
+
+        $table  = $this->getRelationsModel()->getTable()->getBase();
+        $column = $this->getTable()->getIdentityColumn();
+
+        $query->join($table . ' AS relations', 'relations.' . $column . ' = tbl.' . $column, 'INNER');
+
+        $state = $context->getState();
+
+        foreach (array_keys($this->_columns) as $column)
+        {
+            if ($state->{$column}) {
+                $query->where(sprintf('relations.%1$s = :%1$s', $column))->bind(array($column => $state->{$column}));
+            }
+        }
+    }
+
+    /**
+     * Insert the model states
+     *
+     * @param KObjectMixable $mixer
+     */
+    public function onMixin(KObjectMixable $mixer)
+    {
+        parent::onMixin($mixer);
+
+        if ($mixer instanceof KModelDatabase)
+        {
+            foreach ($this->_columns as $name => $filter) {
+                $mixer->getState()->insert($name, $filter);
+            }
+        }
+    }
+
+    /**
+     * Relations Model getter.
+     *
+     * @return KModelInterface
+     */
+    public function getRelationsModel()
+    {
+        if (!$this->_model instanceof KModelInterface)
+        {
+            $identifier = $this->_model;
+
+            if (is_string($identifier))
+            {
+                if (strpos($identifier, '.') === false)
+                {
+                    $identifier = $this->getMixer()->getIdentifier()->toArray();
+                    $identifier['name'] = $this->_model;
+                }
+
+                $identifier = $this->getIdentifier($identifier);
+            }
+
+            $this->_model = $this->getObject($identifier, array(
+                'relation_column' => $this->getTable()
+                                          ->getIdentityColumn()
+            ));
+        }
+
+        return $this->_model;
+    }
+}
