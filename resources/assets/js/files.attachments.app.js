ba942ebdcf0611f5cb52f185ae12f68f9e15d1d7
Arunas Mazeika <arunas@timble.net>
2016-03-11 11:52:43 +0100

    re #83 Added attachments app.

---
+++ resources/assets/js/files.attachments.app.js
@@ -0,0 +1,196 @@
+/**
+ * Nooku Framework - http://nooku.org/framework
+ *
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ */
+
+if (!Attachments) Attachments = {};
+
+Attachments.App = new Class({
+    Extends: Files.App,
+    Implements: [Events, Options],
+    cookie: false,
+    attachments: {},
+    options: {
+        url: null,
+        pathway: false,
+        persistent: false,
+        types: ['file', 'image'],
+        preview:  'files-preview',
+        state: {
+            defaults: {
+                limit: 0,
+                offset: 0
+            }
+        },
+        grid: {
+            spinner_container: 'attachments-spinner',
+            cookie: false,
+            layout: 'attachments',
+            element: 'attachments-grid'
+        },
+        history: {
+            enabled: false
+        },
+        uploader_dialog: false,
+        folder_dialog: false,
+        copy_dialog: false,
+        move_dialog: false,
+        onAfterNavigate: function(path) {
+            // Do nothing
+        }
+    },
+    initialize: function(options) {
+        this.url = options.url;
+
+        this.parent(options);
+
+        this.preview = document.id(this.options.preview);
+
+        var app = this;
+
+        if (callback = options.callback)
+        {
+            if (typeof window.parent[callback] == 'function') {
+                window.parent[callback](app);
+            }
+        }
+    },
+    navigate: function(path, type, revalidate_cache, response) {
+        this.fireEvent('beforeNavigate', [path, type]);
+
+        var that = this;
+
+        if (this.url)
+        {
+            that.grid.reset(); // Flush current content.
+
+            this.grid.spin();
+
+            new Request.JSON({
+                url: this.url,
+                method: 'get',
+                onSuccess: function(response)
+                {
+                    that.grid.insertRows(response.entities);
+                    that.grid.unspin();
+                }
+            }).send();
+        }
+
+        this.fireEvent('afterNavigate', [path, type]);
+    },
+    setPaginator: function() {
+    },
+    setGrid: function() {
+        var opts = this.options.grid;
+        var that = this;
+
+        Object.append(opts, {
+            'onClickAttachment': function (e) {
+                var target = document.id(e.target),
+                    node = target.getParent('.files-node-shadow') || target.getParent('.files-node');
+
+                node.getParent().getChildren().removeClass('active');
+                node.addClass('active');
+                var row = node.retrieve('row');
+                var copy = Object.append({}, row);
+                copy.template = 'details_attachment';
+
+                that.preview.empty();
+
+                copy.render('attachments').inject(that.preview);
+
+                if (copy.thumbnail) {
+                    that.preview.getElement('img').set('src', copy.thumbnail).show();
+                }
+
+                that.grid.selected = row.name;
+            }
+        });
+
+        this.grid = new Attachments.Grid(this.options.grid.element, opts);
+
+    }
+});
+
+Attachments.Grid = new Class({
+    Extends: Files.Grid,
+    select: function(node) {
+        if (typeof node === 'string') {
+            node = this.nodes.get(node);
+        }
+
+        var handler = 'click' + node.type.capitalize();
+
+        this.fireEvent(handler, {target: node.element.getElement('a.navigate')});
+    },
+    unselect: function() {
+        this.container.getElements('.files-node').removeClass('active');
+        this.selected = null;
+    },
+    attachEvents: function() {
+        var that = this;
+        this.container.addEvent('click:relay(.files-attachment a.navigate)', function(e) {
+            e.stop();
+            that.fireEvent('clickAttachment', arguments);
+        });
+    },
+    /**
+     * Insert multiple rows, possibly coming from a JSON request
+     */
+    insertRows: function(rows) {
+        this.fireEvent('beforeInsertRows', {rows: rows});
+
+        Object.each(rows, function(row) {
+            var item = new Files.Attachment(row);
+            this.insert(item, 'last');
+        }.bind(this));
+
+        if (this.options.icon_size) {
+            this.setIconSize(this.options.icon_size);
+        }
+
+        this.fireEvent('afterInsertRows', {rows: rows});
+    },
+});
+
+Files.Template.Attachments = new Class({
+    initialize: function(html) {
+        return new Element('div', {html: html}).getFirst();
+    }
+});
+
+Files.Attachment = new Class({
+    Extends: Files.Row,
+
+    type: 'attachment',
+    template: 'attachment',
+    initialize: function(object, options) {
+        this.parent(object, options);
+
+        this.size = new Files.Filesize(this.file.metadata.size);
+        this.filetype = Files.getFileType(this.file.metadata.extension);
+    },
+    delete: function(success, failure) {
+        // Do nothing, just call the success event handler.
+        if (typeof success == 'function') {
+            success();
+        }
+    },
+    getAttachedDate: function(formatted) {
+        if (this.attached_on_timestamp) {
+            var date = new Date();
+            date.setTime(this.attached_on_timestamp*1000);
+            if (formatted) {
+                return date.getDate()+' '+Koowa.Date.getMonthName(date.getMonth()+1, true)+' '+date.getFullYear();
+            } else {
+                return date;
+            }
+        }
+
+        return null;
+    }
+});
