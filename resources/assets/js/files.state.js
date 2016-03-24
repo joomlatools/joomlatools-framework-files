1becee9b514b3f8cae8ad1649f58f8e4fd0c9d0b
Ercan Ozkaya <ercan@timble.net>
2016-03-24 16:39:37 +0200

    Change repo paths

--- resources/assets/js/files.state.js
+++ resources/assets/js/files.state.js
@@ -3,7 +3,7 @@
  *
  * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ * @link		http://github.com/joomlatools/joomlatools-framework-files for the canonical source repository
  */
 
 if (!Files) var Files = {};
--- view/files/tmpl/scripts.html.php
+++ view/files/tmpl/scripts.html.php
@@ -4,7 +4,7 @@
  *
  * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://github.com/nooku/nooku-files for the canonical source repository
+ * @link		http://github.com/joomlatools/joomlatools-framework-files for the canonical source repository
  */
 defined('KOOWA') or die( 'Restricted access' );
 
