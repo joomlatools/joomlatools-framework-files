6d0d441d4ae0d2fcc311db6d536f8164e8e80c53
Ercan Ozkaya <ercan@timble.net>
2016-04-06 15:46:57 +0300

    re #227: Use correct prefix

--- template/helper/paginator.php
+++ template/helper/paginator.php
@@ -13,7 +13,7 @@
  * @author  Ercan Ozkaya <https://github.com/ercanozkaya>
  * @package Koowa\Component\Files
  */
-class ComFilesTemplateHelperPaginator extends ComKoowaTemplateHelperPaginator
+class ComFilesTemplateHelperPaginator extends KTemplateHelperPaginator
 {
     /**
      * Render item pagination
