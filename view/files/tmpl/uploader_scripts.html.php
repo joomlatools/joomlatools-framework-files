f1b444f6a1ac23a50683f4b8821e9667c487a605
Robin Poort <robin@timble.net>
2016-09-07 13:00:07 +0200

    #9: Improvements
    - Adding extra modifiers for uploader
    - Comment out compiling error CSS for files.css

--- view/files/tmpl/uploader_scripts.html.php
+++ view/files/tmpl/uploader_scripts.html.php
@@ -59,186 +59,189 @@ $server_limit = ComFilesModelEntityContainer::getServerUploadLimit();
 kQuery.koowa.uploader.server_limit = <?= json_encode($server_limit) ?>;
 </script>
 
-<!-- Uploader content box -->
-<script data-inline type="text/html" class="js-uploader-template" data-name="content-box">
-    <div class="k-upload__body-default">
-        <div class="k-upload__content-wrapper">
-            <div class="k-upload__buttons">
-                <button type="button" class="k-upload__button js-choose-files"
-                        data-caption-update="<?= escape(translate('Update')) ?>">
-                    <?= translate('Upload') ?>
-                </button>
-                <button type="button" class="k-upload__button k-upload__button--upload js-start-upload">
-                    <?= translate('Start') ?>
-                </button>
-                <button class="k-upload__button js-stop-upload disabled">
-                    <?= translate('Stop') ?>
-                </button>
-                <span class="k-upload__divider">/</span>
-            </div>
-            <div class="k-upload__content">
-                <div class="js-content"></div>
-            </div>
-        </div>
-    </div>
-</script>
+<div class="k-dynamic-content-holder">
 
-<!-- Error box -->
-<script data-inline type="text/html" class="js-uploader-template" data-name="error-box">
-    <div class="k-upload__body-message">
-        <div class="k-upload__message k-upload__message--error">
-            <div class="k-upload__message__content">
-                <div class="k-upload__message__body js-message-body"></div>
-                <div class="k-upload__message__button"><button type="button" class="k-upload__button js-close-error"><?= translate('OK') ?></button></div>
-            </div>
-        </div>
-    </div>
-</script>
-
-<!-- Extra info -->
-<script data-inline type="text/html" class="js-uploader-template" data-name="info-box">
-    <div class="k-upload__body-info">
-        <div class="k-upload__info">
-            <div class="k-upload__info__content">
-                <div class="k-upload__info__body">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th class="k-upload__clear-queue" width="1%" style="width: 1%;"><a href="#" class="k-uploader__clear-queue js-clear-queue"><?= translate('Clear queue') ?></a></th width="1">
-                            <th width="1%" style="width: 1%;"></th>
-                            <th width="1%" style="width: 1%;"><?= translate('Size') ?></th>
-                            <th width="99%" style="width: 99%;"><?= translate('Title') ?></th>
-                        </tr>
-                        </thead>
-                        <tbody class="js-filelist-multiple"></tbody>
-                    </table>
+    <!-- Uploader content box -->
+    <script data-inline type="text/html" class="js-uploader-template" data-name="content-box">
+        <div class="k-upload__body-default">
+            <div class="k-upload__content-wrapper">
+                <div class="k-upload__buttons">
+                    <button type="button" class="k-upload__button js-choose-files"
+                            data-caption-update="<?= escape(translate('Update')) ?>">
+                        <?= translate('Upload') ?>
+                    </button>
+                    <button type="button" class="k-upload__button k-upload__button--upload js-start-upload">
+                        <?= translate('Start') ?>
+                    </button>
+                    <button class="k-upload__button js-stop-upload disabled">
+                        <?= translate('Stop') ?>
+                    </button>
+                    <span class="k-upload__divider">/</span>
                 </div>
-                <div class="k-upload__info__button">
+                <div class="k-upload__content">
+                    <div class="js-content"></div>
                 </div>
             </div>
         </div>
-    </div>
-</script>
+    </script>
 
-<!-- Progress bar -->
-<script data-inline type="text/html" class="js-uploader-template" data-name="progress-bar">
-    <div class="k-upload__progress progress progress-striped">
-        <div class="k-upload__progress__bar bar" style="width: 0"></div>
-    </div>
-</script>
+    <!-- Error box -->
+    <script data-inline type="text/html" class="js-uploader-template" data-name="error-box">
+        <div class="k-upload__body-message">
+            <div class="k-upload__message k-upload__message--error">
+                <div class="k-upload__message__content">
+                    <div class="k-upload__message__body js-message-body"></div>
+                    <div class="k-upload__message__button"><button type="button" class="k-upload__button js-close-error"><?= translate('OK') ?></button></div>
+                </div>
+            </div>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="single-file">
-    <div class="k-upload__files">
-        <div class="k-upload__file-list">
-            <div id="{{=it.id}}" class="js-uploader-file">
+    <!-- Extra info -->
+    <script data-inline type="text/html" class="js-uploader-template" data-name="info-box">
+        <div class="k-upload__body-info">
+            <div class="k-upload__info">
+                <div class="k-upload__info__content">
+                    <div class="k-upload__info__body">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th class="k-upload__clear-queue" width="1%" style="width: 1%;"><a href="#" class="k-uploader__clear-queue js-clear-queue"><?= translate('Clear queue') ?></a></th width="1">
+                                <th width="1%" style="width: 1%;"></th>
+                                <th width="1%" style="width: 1%;"><?= translate('Size') ?></th>
+                                <th width="99%" style="width: 99%;"><?= translate('Title') ?></th>
+                            </tr>
+                            </thead>
+                            <tbody class="js-filelist-multiple"></tbody>
+                        </table>
+                    </div>
+                    <div class="k-upload__info__button">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </script>
+
+    <!-- Progress bar -->
+    <script data-inline type="text/html" class="js-uploader-template" data-name="progress-bar">
+        <div class="k-upload__progress progress progress-striped">
+            <div class="k-upload__progress__bar bar" style="width: 0"></div>
+        </div>
+    </script>
+
+    <script data-inline type="text/html" class="js-uploader-template" data-name="single-file">
+        <div class="k-upload__files">
+            <div class="k-upload__file-list">
+                <div id="{{=it.id}}" class="js-uploader-file">
+                    <span class="js-file-name-container">{{=it.name}}</span>
+                    {{?it.size}}
+                    , <span class="js-file-size-container">{{=it.size}}</span>
+                    {{?}}
+                </div>
+            </div>
+        </div>
+        <div class="k-upload__drop-message">
+            <?= translate('Drop another file to update') ?>
+        </div>
+
+    </script>
+
+    <script data-inline type="text/html" class="js-uploader-template" data-name="multiple-files">
+        <tr id="{{=it.id}}" class="js-uploader-file">
+            <td class="k-upload__file-status-wrapper">
+                <span class="k-upload__file-status js-file-status is-in-queue"><?= translate('in queue') ?></span>
+            </td>
+            <td>
+                <a class="k-upload__remove-button js-remove-file">x</a>
+            </td>
+            <td>
+                <span class="js-file-size-container">{{=it.size}}</span>
+            </td>
+            <td class="k-upload__overflow">
                 <span class="js-file-name-container">{{=it.name}}</span>
-                {{?it.size}}
-                , <span class="js-file-size-container">{{=it.size}}</span>
-                {{?}}
-            </div>
+            </td>
+        </tr>
+    </script>
+
+    <script data-inline type="text/html" class="js-uploader-template" data-name="upload-pending">
+        <div>
+            <?= translate('{total} files in the queue', array(
+                'total' => '{{=it.total}}'
+            )) ?>
+            <a class="k-upload__details-button js-open-info">
+                <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
+                <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
+            </a>
         </div>
-    </div>
-    <div class="k-upload__drop-message">
-        <?= translate('Drop another file to update') ?>
-    </div>
+        <div class="k-upload__drop-message">
+            <?= translate('Drop files to add to the queue') ?>
+        </div>
+    </script>
 
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="uploading">
+        <div>
+            <?= translate('Uploading {total} files, {remaining} to go', array(
+                'total' => '{{=it.total}}',
+                'remaining' => '{{=it.remaining}}'
+            ), '</span>') ?>
+            {{? it.failed > 0 }}
+            -
+            <?= translate('{failed} errors', array(
+                'failed' => '{{=it.failed}}'
+            )) ?>
+            {{?}}
+            <a class="k-upload__details-button js-open-info">
+                <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
+                <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
+            </a>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="multiple-files">
-    <tr id="{{=it.id}}" class="js-uploader-file">
-        <td class="k-upload__file-status-wrapper">
-            <span class="k-upload__file-status js-file-status is-in-queue"><?= translate('in queue') ?></span>
-        </td>
-        <td>
-            <a class="k-upload__remove-button js-remove-file">x</a>
-        </td>
-        <td>
-            <span class="js-file-size-container">{{=it.size}}</span>
-        </td>
-        <td class="k-upload__overflow">
-            <span class="js-file-name-container">{{=it.name}}</span>
-        </td>
-    </tr>
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="upload-finished">
+        <div>
+            <?= translate('Uploaded {total} files', array(
+                'total' => '{{=it.total}}'
+            )) ?>
+            {{? it.failed > 0 }}
+            -
+            <?= translate('{failed} errors', array(
+                'failed' => '{{=it.failed}}'
+            )) ?>
+            {{?}}
+            <a class="k-upload__details-button js-open-info">
+                <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
+                <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
+            </a>
+        </div>
+        <div class="k-upload__drop-message">
+            <?= translate('Drop more files here') ?>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="upload-pending">
-    <div>
-        <?= translate('{total} files in the queue', array(
-            'total' => '{{=it.total}}'
-        )) ?>
-        <a class="k-upload__details-button js-open-info">
-            <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
-            <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
-        </a>
-    </div>
-    <div class="k-upload__drop-message">
-        <?= translate('Drop files to add to the queue') ?>
-    </div>
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="empty-single">
+        <div class="k-upload__drop-message">
+            <?= translate('Drop a file here') ?>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="uploading">
-    <div>
-        <?= translate('Uploading {total} files, {remaining} to go', array(
-            'total' => '{{=it.total}}',
-            'remaining' => '{{=it.remaining}}'
-        ), '</span>') ?>
-        {{? it.failed > 0 }}
-        -
-        <?= translate('{failed} errors', array(
-            'failed' => '{{=it.failed}}'
-        )) ?>
-        {{?}}
-        <a class="k-upload__details-button js-open-info">
-            <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
-            <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
-        </a>
-    </div>
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="empty-multiple">
+        <div class="k-upload__drop-message">
+            <?= translate('Drop files here') ?>
+        </div>
+        <div class="k-upload__select-message">
+            <?= translate('Select files to upload') ?>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="upload-finished">
-    <div>
-        <?= translate('Uploaded {total} files', array(
-            'total' => '{{=it.total}}'
-        )) ?>
-        {{? it.failed > 0 }}
-        -
-        <?= translate('{failed} errors', array(
-            'failed' => '{{=it.failed}}'
-        )) ?>
-        {{?}}
-        <a class="k-upload__details-button js-open-info">
-            <span class="k-upload__details-button__view"><?= translate('show queue') ?></span>
-            <span class="k-upload__details-button__close"><?= translate('hide queue') ?></span>
-        </a>
-    </div>
-    <div class="k-upload__drop-message">
-        <?= translate('Drop more files here') ?>
-    </div>
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="drop-message-single">
+        <div class="k-uploader-drop-visual">
+            <span><?= translate('Drop a file here') ?></span>
+        </div>
+    </script>
 
-<script data-inline type="text/html" class="js-uploader-template" data-name="empty-single">
-    <div class="k-upload__drop-message">
-        <?= translate('Drop a file here') ?>
-    </div>
-</script>
-
-<script data-inline type="text/html" class="js-uploader-template" data-name="empty-multiple">
-    <div class="k-upload__drop-message">
-        <?= translate('Drop files here') ?>
-    </div>
-    <div class="k-upload__select-message">
-        <?= translate('Select files to upload') ?>
-    </div>
-</script>
-
-<script data-inline type="text/html" class="js-uploader-template" data-name="drop-message-single">
-    <div class="k-uploader-drop-visual">
-        <span><?= translate('Drop a file here') ?></span>
-    </div>
-</script>
-
-<script data-inline type="text/html" class="js-uploader-template" data-name="drop-message-multiple">
-    <div class="k-uploader-drop-visual">
-        <span><?= translate('Drop files here') ?></span>
-    </div>
-</script>
+    <script data-inline type="text/html" class="js-uploader-template" data-name="drop-message-multiple">
+        <div class="k-uploader-drop-visual">
+            <span><?= translate('Drop files here') ?></span>
+        </div>
+    </script>
+</div>
