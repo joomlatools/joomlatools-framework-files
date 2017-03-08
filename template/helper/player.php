112c39b239a7a7d7332570b9cf6fafeb545d5be7
Rastin Mehr <rastin@timble.net>
2017-03-08 22:59:53 +0000

    #26 - Add the plyr library - consolidated supported formats in one array - cleaned up legacy function load() - removed singleton creation code block to _initialize method

--- template/helper/player.php
+++ template/helper/player.php
@@ -8,44 +8,25 @@
 
 class ComFilesTemplateHelperPlayer extends KTemplateHelperAbstract
 {
-    protected static $HTML5_VIDEO_FORMATS = array('mp4', 'webm', 'ogg');
+    protected static $_SUPPORTED_FORMATS = array(
+        'audio' => array('aac', 'mp3', 'ogg', 'flac','x-flac', 'wave', 'wav', 'x-wav', 'x-pn-wav'),
+        'video' => array('mp4', 'webm', 'ogg')
+    );
 
-    protected static $HTML5_AUDIO_FORMATS = array('aac', 'mp3', 'ogg', 'flac','x-flac', 'wave', 'wav', 'x-wav', 'x-pn-wav');
-
-    protected static $declared = false;
-
-    /*
-    public function load()
-    {
-        static $html = '';
-
-        if (! self::$declared && $html == '') {
-            $html .= '<ktml:script src="media://koowa/com_files/js/plyr/plyr.js" />';
-            $html .= '<script>kQuery(function($){plyr.setup()});</script>';
-            $html .= '<ktml:style src="media://koowa/com_files/css/plyr.css" />';
-        }
-
-        return $html;
-    }
-    */
+    protected static $_DECLARED = false;
 
     protected function _initialize(KObjectConfig $config)
     {
-        $this->addDeclarations();
-
-        parent::_initialize($config);
-    }
-
-    public function addDeclarations()
-    {
-        if (! self::$declared) {
+        if (! self::$_DECLARED) {
 
             $document = JFactory::getDocument();
             $document->addScript('media/koowa/com_files/js/plyr/plyr.js');
             $document->addScriptDeclaration('jQuery(function($){plyr.setup()});');
             $document->addStylesheet('media/koowa/com_files/css/plyr.css');
 
-            self::$declared = true;
+            self::$_DECLARED = true;
         }
+
+        parent::_initialize($config);
     }
 }
