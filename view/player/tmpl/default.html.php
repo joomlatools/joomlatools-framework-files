4ee63ca0194348c6b34a5284e23038af2af5c54c
Rastin Mehr <rastin@timble.net>
2017-07-29 06:56:09 +0000

    #45 - AV Player Google Analytics - added javascript code that pushes GA events by listening to pause event from players
    NOTE: this is a proof of concept and needs polishing

--- view/player/tmpl/default.html.php
+++ view/player/tmpl/default.html.php
@@ -16,6 +16,43 @@ defined('KOOWA') or die;
 <script>
     kQuery(function($){
         plyr.setup();
+
+        $(document).on('pause', function(event) {
+            var plyr = event.detail.plyr;
+
+            // If they've played over 3 seconds, then consider it played
+            // This is the same timing convention used by Facebook, Instagram, and Twitter
+            if (plyr.getCurrentTime() > 3.0) {
+
+                var media = $(plyr.getMedia());
+
+                var category = media.data('category');
+                var action = 'Play ' + plyr.getType();
+
+                var title = media.data('title') || '';
+                var id = parseInt(media.data('media-id'), 10) || -1;
+                var label = id + ". " + title;
+                var time = Math.ceil(plyr.getCurrentTime() * 10) / 10;
+
+                //console.log(category, action, label, time);
+
+                if (typeof window.GoogleAnalyticsObject !== 'undefined' && typeof window[window.GoogleAnalyticsObject] !== 'undefined') {
+                    window[window.GoogleAnalyticsObject]('send', 'event', category, action, label, {
+                        'time' : time
+                    });
+                }
+                else if (typeof _gaq !== 'undefined' && typeof _gat !== 'undefined') {
+                    if (_gat._getTrackers().length) {
+                        _gaq.push(function () {
+                            var tracker = _gat._getTrackers()[0];
+                            tracker._trackEvent(category, action, label, {
+                                'time' : time
+                            });
+                        });
+                    }
+                }
+            }
+        });
     });
 </script>
 
