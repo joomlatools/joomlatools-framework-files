bcda9816f6cd894d9e9b1c2c477fc22abce73b60
Arunas Mazeika <arunas@timble.net>
2017-11-28 10:40:42 +0100

    #49 Add attachments files table

---
+++ database/table/attachments_files.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Joomlatools Framework - https://www.joomlatools.com/developer/framework/
+ *
+ * @copyright	Copyright (C) 2011 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/joomlatools-framework-files for the canonical source repository
+ */
+
+/**
+ * Attachments Files Database Table
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesDatabaseTableAttachments_files extends KDatabaseTableAbstract
+{
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'column_map'      => array(
+                'container' => 'files_container_id',
+            ),
+            'behaviors'       => array(
+                'identifiable',
+                'creatable',
+                'modifiable',
+                'lockable'
+            )
+        ));
+
+        parent::_initialize($config);
+    }
+}
