b7a84d9dede76424a87c52860e18c5b32c6b9839
Arunas Mazeika <arunas@timble.net>
2018-04-13 13:52:03 +0200

    #59 Re-factor beahvior
    - Add containsImage method to identify thumbnailable entities
    - Add thumbnailable_extensions property for defining thumbnailable files

--- database/behavior/thumbnailable.php
+++ database/behavior/thumbnailable.php
@@ -15,11 +15,57 @@
  */
 class ComFilesDatabaseBehaviorThumbnailable extends KDatabaseBehaviorAbstract
 {
+    /**
+     * @var array A list of files extensions for which thumbnails may be generated
+     */
+    protected $_thumbnailable_extensions;
+
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_thumbnailable_extensions = KObjectConfig::unbox($config->thumbnailable_extensions);
+    }
+
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array('thumbnailable_extensions' => ComFilesModelEntityFile::$image_extensions));
+
+        parent::_initialize($config);
+    }
+
+    /**
+     * Tells if a file contains an image
+     *
+     * Files containing images are thumbnailable, meaning that a thumbnail may be generated for them
+     *
+     * @return bool true if it contains an image, false otherwise
+     */
+    public function containsImage()
+    {
+        $result = false;
+        $mixer  = $this->getMixer();
+
+        if ($mixer instanceof ComFilesModelEntityFile && !$mixer->isNew()) {
+            $result = in_array($mixer->extension, $this->_thumbnailable_extensions);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Thumbnail getter
+     *
+     * @param string $version The version model state value
+     *
+     * @return ComFilesModelEntityThumbnails|false The thumbnails entity object, false if an empty entity is returned or
+     *                                             if the model could not be fetched
+     */
     public function getThumbnail($version =  null)
     {
         $thumbnail = false;
 
-        if (!$this->isNew() && $container = $this->thumbnails_container_slug)
+        if ($container = $this->thumbnails_container_slug)
         {
             $model = $this->getObject('com:files.model.thumbnails')->container($container)->source($this->uri);
 
