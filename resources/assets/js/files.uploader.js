923f8ca2f1d4c6f6b915b0c760e53368eacf4b64
Ercan Ozkaya <ercan@timble.net>
2018-06-12 11:53:57 +0300

    Revert "Merge pull request #74 from joomlatools/feature/73-hidden-files"
    This reverts commit 2036fec9fb061e68004c01020b5e57a6a646e5a9, reversing
    changes made to df591f0f1d4b7302f16bc811dfd566baaa4d42c5.

--- resources/assets/js/files.uploader.js
+++ resources/assets/js/files.uploader.js
@@ -81,12 +81,6 @@ if (!Files) var Files = {};
                     }
                     Files.app.fireEvent('uploadFile', [row]);
                 }
-
-                if ( document.getElementById('files-grid').hasClass('k-is-hidden') ) {
-                    document.getElementById('empty-state').addClass('k-is-hidden');
-                    document.getElementById('files-grid').removeClass('k-is-hidden');
-                }
-
             }
 
         }, options);
